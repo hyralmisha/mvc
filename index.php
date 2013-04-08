92837b3ee5e73d9ce909ab01618e99075f966b3c
hyralmisha <hyralmm@ukr.net>
2013-04-08 17:38:50 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+// підключаємо файли конфігурації
+require_once 'config/model.php';
+require_once 'config/view.php';
+require_once 'config/controller.php';
+
+        require_once 'config/route.php';
+        Route::start(); // запускаем маршрутизатор
+?>
+        
