5131fd61242d09b7f560120c0233998165e157b4
hyralmisha <hyralmm@ukr.net>
2013-04-30 08:50:49 +0300

    v04-30
    Signed-off-by: hyralmisha <hyralmm@ukr.net>

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
-require_once 'config/headers.php';        
-require_once 'config/model.php';
-require_once 'config/view.php';
-require_once 'config/controller.php';
+require_once 'config/headers.php'; //заголовки
+require_once 'config/autoload.php'; //масив класів
+//Визначаємо функцію  __autoload
+function __autoload($class) {
+    global $_autoload;
+    
+    if (isset($_autoload[$class])) {
+        include($_autoload[$class]);
+    } 
+}
 
-        require_once 'config/route.php';
-        Route::start(); // запускаем маршрутизатор
-		
+Route::start(); // запускаем маршрутизатор
 ?>
